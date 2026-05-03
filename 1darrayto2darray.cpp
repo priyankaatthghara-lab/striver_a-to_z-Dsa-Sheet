fd3d943c72b5cd4c096fa4244d96a0eb4a9efcf6
Priyanka Kumari <Priyankaatthghara@gmail.com>
2026-05-03 12:54:02 +0530

    add1darrayto2darray

---
+++ 1darrayto2darray.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+void convertarray(int *array,int n){
+    int m=3; int p=2;
+    int matrix[m][p]; int idx=0;
+    if (sizeof(array)!= m*p)
+    {
+       cout<<" 2D matrix is not possible for this array";
+    }
+    
+    for (int i = 0; i <m; i++)
+    {
+        for (int j = 0; j < p; j++)
+        {
+           matrix[i][j]=array[idx];
+           idx++;
+        }
+        
+    }
+    for (int i = 0; i <m; i++)
+    {
+       for (int j = 0; j < p; j++)
+       {
+        cout<<matrix[i][j]<<" ";
+       }
+       cout<<"\n";
+    }
+    
+    
+}
+int main(){
+    int arr[6]={1,2,3,4,5,6};
+    int n=sizeof(arr)/sizeof(int);
+    convertarray(arr,n);
+    return 0;
+}
