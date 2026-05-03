148b860119cbdda8a43d7baea7c43de2cfd4431a
Priyanka Kumari <Priyankaatthghara@gmail.com>
2026-05-03 13:14:32 +0530

    addedoptimised

--- 1darrayto2darray.cpp
+++ 1darrayto2darray.cpp
@@ -1,3 +1,11 @@
+/*
+Leetcode problem:2022. Convert 1D Array Into 2D Array
+Brute forces uses two loops to fill the 2D array, but we can do it in one loop by using the formula:
+row = i / number_of_columns
+column = i % number_of_columns
+This way, we can directly calculate the position in the 2D array for each element in the 1D array without needing nested loops. The time complexity of this approach is O(n), where n is the number of elements in the 1D array, since we are iterating through it once. The space complexity is O(m*p) for the 2D array, where m is the number of rows and p is the number of columns.
+*/
+
 #include<iostream>
 using namespace std;
 void convertarray(int *array,int n){
@@ -7,15 +15,13 @@ void convertarray(int *array,int n){
     {
        cout<<" 2D matrix is not possible for this array";
     }
-    for (int i = 0; i <m; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < p; j++)
-        {
-           matrix[i][j]=array[idx];
-           idx++;
-        }
-        
+        int r=i/p;
+        int c=i%p;
+        matrix[r][c]=array[i];
     }
+    
     for (int i = 0; i <m; i++)
     {
        for (int j = 0; j < p; j++)
